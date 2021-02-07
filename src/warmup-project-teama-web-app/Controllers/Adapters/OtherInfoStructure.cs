2b79a7fc60205af06c404783975812b69d87c233
Emily Chang <chang.emily99@gmail.com>
2021-02-07 12:20:42 -0600

    lol actual quick fix for content-type

--- src/warmup-project-teama-web-app/Controllers/Adapters/OtherInfoStructure.cs
+++ src/warmup-project-teama-web-app/Controllers/Adapters/OtherInfoStructure.cs
@@ -1,5 +1,8 @@
+using Newtonsoft.Json;
+
 public class OtherInfoStructure
 {
+    [JsonProperty(PropertyName = "content-type")]
     public string contentType { get; set; } 
     public string value { get; set; } 
     public string name { get; set; }
