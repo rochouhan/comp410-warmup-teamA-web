6ed169b670d5a9fd9a44766c0f5b98bdff528d3d
Rohit Chouhan <ro.chouhan@gmail.com>
2021-02-09 11:41:24 -0600

    Added comments to Session Extensions

--- src/warmup-project-teama-web-app/Models/Session/SessionExtensions.cs
+++ src/warmup-project-teama-web-app/Models/Session/SessionExtensions.cs
@@ -4,13 +4,30 @@ using Microsoft.AspNetCore.Http;
 
 namespace warmup_project_teama_web_app.Models.Session
 {
+    /// <summary>
+    /// Extensions of the Sessions class to handle saving objects.
+    /// </summary>
     public static class SessionExtensions
     {
+        /// <summary>
+        /// Saves object in session.
+        /// </summary>
+        /// <typeparam name="T">Object type to save</typeparam>
+        /// <param name="session">Session variable of the HTTP context</param>
+        /// <param name="key">Name of the variable to save</param>
+        /// <param name="value">Actual object to save</param>
         public static void Set<T>(this ISession session, string key, T value)
         {
             session.SetString(key, JsonSerializer.Serialize(value));
         }
 
+        /// <summary>
+        /// Gets object from session.
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <param name="session">Session variable of the HTTP context</param>
+        /// <param name="key">Name of the variable to get</param>
+        /// <returns>The object saved at Session[key], or default otherwise</returns>
         public static T Get<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
